37b76e3de2fd7939ef165e1b6d44c0a2e0a82504
kevinarias1337 <kevinlh1337@gmail.com>
2023-05-10 10:20:39 -0500

    Se agrega el contenido y el footer a la web y se implementa un Slider Gallery con la lib Framer Motion

---
+++ react-project-cero/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className='Footer'>
+                <p> &copy; Papa's Pizzeria, todos los derechos reservados, 2023.</p>
+                <div className='clearfix'></div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
